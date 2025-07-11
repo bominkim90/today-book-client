36ae7b05d1ce9e2351169da2788ff6e846eba6dc
bominkim90 <bomin97@naver.com>
2025-07-11 14:20:57 +0900

    feat: 로그아웃 컨펌 모달 추가

--- src/pages/my/main/LogoutButton.tsx
+++ src/pages/my/main/LogoutButton.tsx
@@ -1,9 +1,12 @@
 import { useNavigate } from 'react-router-dom';
 import { useLogout } from '../../../hooks/useLogout';
+import ConfirmModal from '../../../components/modals/ConfirmModal';
+import { useState } from 'react';
 
 export default function LogoutButton() {
   const navigate = useNavigate();
   const { mutate: logout } = useLogout();
+  const [showConfirmModal, setShowConfirmModal] = useState(false);
 
   async function handleLogout() {
     logout(undefined, {
@@ -17,8 +20,17 @@ export default function LogoutButton() {
   }
 
   return (
-    <button className="w-full h-[48px] bg-mainGray text-textSecondary rounded-sm" onClick={handleLogout}>
+    <>
+    <button className="w-full h-[48px] bg-mainGray text-textSecondary rounded-sm" onClick={() => setShowConfirmModal(true)}>
       로그아웃
     </button>
+    {showConfirmModal && 
+    <ConfirmModal 
+      title="로그아웃"
+      message="로그아웃 하시겠습니까?"
+      onConfirm={handleLogout}
+      onCancel={() => setShowConfirmModal(false)}
+    />}
+    </>
   );
 }
